3d5cab4b04def6fbb0bbfb1759d8eccf65160e15
Dimitri 'skypers' Sabadie <dimitri.sabadie@gmail.com>
2013-06-04 09:13:22 +0200

    Vertex shader of room.

--- src/fsm/room-vs.glsl
+++ src/fsm/room-vs.glsl
@@ -1,3 +1,22 @@
 #version 330 core
 
+uniform mat4 proj;
 
+flat out vec3 vno; /* normal as vertex shader output */
+
+/* cube vertices */
+const vec3[8] v = vec3[] (
+    vec3(-1.,  1.,  1.)
+  , vec3( 1.,  1.,  1.)
+  , vec3( 1., -1.,  1.)
+  , vec3(-1., -1.,  1.)
+  , vec3(-1.,  1., -1.)
+  , vec3( 1.,  1., -1.)
+  , vec3( 1., -1., -1.)
+  , vec3(-1., -1., -1.)
+);
+
+void main() {
+  gl_Position = proj * vec4(v[gl_VertexID], 1.);
+  vno = floor(v[gl_VertexiID]);
+}
