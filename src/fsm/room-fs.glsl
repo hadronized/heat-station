1bed5d98c956d3516c93918683757219d2183cc8
Dimitri 'skypers' Sabadie <dimitri.sabadie@gmail.com>
2013-06-04 09:30:50 +0200

    what.

--- src/fsm/room-fs.glsl
+++ src/fsm/room-fs.glsl
@@ -1,3 +1,13 @@
 #version 330 core
 
+uniform mat4 proj; /* used to retrieve camera position */
+
+in vec3 vco;      /* vertex shader space coordinates */
+flat in vec3 vno; /* vertex shader normal */
+
+out vec4 frag;
+
+void main() {
+  /* first, simple diffuse phong with origined light */
+  frag = 
 
--- src/fsm/room-vs.glsl
+++ src/fsm/room-vs.glsl
@@ -2,6 +2,7 @@
 
 uniform mat4 proj;
 
+out vec3 vco;      /* space coordinates as vertex shader output */
 flat out vec3 vno; /* normal as vertex shader output */
 
 /* cube vertices */
@@ -17,6 +18,8 @@ const vec3[8] v = vec3[] (
 );
 
 void main() {
-  gl_Position = proj * vec4(v[gl_VertexID], 1.);
-  vno = floor(v[gl_VertexiID]);
+  vco = v[gl_VertexID];
+  vno = floor(vco);
+
+  gl_Position = proj * vec4(vco, 1.);
 }
