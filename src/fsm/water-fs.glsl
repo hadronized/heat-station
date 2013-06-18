3bf3a1bb2a624fd54a69682348b18f9201529576
Dimitri 'skypers' Sabadie <dimitri.sabadie@gmail.com>
2013-06-18 22:13:40 +0200

    Ok now let’s tessellate that water thing!

--- src/fsm/water-fs.glsl
+++ src/fsm/water-fs.glsl
@@ -8,4 +8,9 @@ out vec4 frag;
 void main() {
   float faceid = float(gl_PrimitiveID / 2);
   frag = vec4(1. - sin(faceid), cos(faceid), 0., 1.);
+
+  /* laser phong */
+  float laserDist = length(vco.xy);
+
+  frag /= laserDist * 0.1;
 }
