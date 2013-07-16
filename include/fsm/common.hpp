2c0524720aba6502e5617f1036a205d6183e4e51
Dimitri 'skypers' Sabadie <dimitri.sabadie@gmail.com>
2013-07-16 22:46:22 +0200

    Common fsm objects.

---
+++ include/fsm/common.hpp
@@ -0,0 +1,21 @@
+#ifndef __FSM_COMMON_HPP
+#define __FSM_COMMON_HPP
+
+#include <math/common.hpp>
+#include <scene/material_manager.hpp>
+#include <tech/deferred_renderer.hpp>
+
+float  const FOVY             = sky::math::PI*70.f/180.f; /* 90 degrees */
+float  const ZNEAR            = 0.0001f;
+float  const ZFAR             = 100.f;
+
+struct Common {
+  sky::tech::DeferredRenderer drenderer;
+  sky::scene::MaterialManager matmngr;
+
+  Common(sky::ushort width, sky::ushort height);
+  ~Common(void) = default;
+};
+
+#endif /* guard */
+
---
+++ src/fsm/common.cpp
@@ -0,0 +1,10 @@
+#include <fsm/common.hpp>
+
+using namespace sky;
+using namespace core;
+using namespace scene;
+using namespace tech;
+
+Common::Common(ushort width, ushort height) :
+    drenderer(width, height) {
+}
