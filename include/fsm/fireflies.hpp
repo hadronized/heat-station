fe805eec8b9ccd816fd80452cdfad290add04545
Dimitri 'skypers' Sabadie <dimitri.sabadie@gmail.com>
2013-07-23 23:23:07 +0200

    No fireflies yet.

---
+++ include/fsm/fireflies.hpp
@@ -0,0 +1,10 @@
+#ifndef __FSM_FIREFLIES_HPP
+#define __FSM_FIREFLIES_HPP
+
+#include <scene/common.hpp>
+
+class Fireflies {
+  Position _
+};
+
+#endif /* guard */
---
+++ src/fsm/fireflies.cpp
@@ -0,0 +1,3 @@
+#include <fsm/fireflies.hpp>
+
+
